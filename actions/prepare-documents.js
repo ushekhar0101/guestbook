21eef966734e76c05010c0e9f0fa49aafd2b1a81
ushekhar0101 <41282052+ushekhar0101@users.noreply.github.com>
2018-07-27 23:02:42 +0530

    Update prepare-documents.js

--- actions/prepare-documents.js
+++ actions/prepare-documents.js
@@ -6,15 +6,9 @@ function main(params) {
     return Promise.reject({error: 'Insufficient Data'});
   }
 
-    entries: params.rows.map((row) => { return {
-	    email-o: row.doc.email
-    }})
-  if (params.email = email-o) {
-	  return Promise.reject({error: 'Already registered Email ID'});
-  }
-	return {
-    	doc: {
-	  createdAt: new Date(),
+  return {
+    doc: {
+      createdAt: new Date(),
 	    name: params.name,
 	    email: params.email,
 	    comment: params.comment,
